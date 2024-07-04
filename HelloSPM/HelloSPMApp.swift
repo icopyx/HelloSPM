ce6d22c01d8a9a857296aa0c8945d3e235fb98dd
langpeu <icopy@naver.com>
2024-07-04 15:17:32 +0900

    Initial Commit

---
+++ HelloSPM/HelloSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSPMApp.swift
+//  HelloSPM
+//
+//  Created by Langpeu on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
