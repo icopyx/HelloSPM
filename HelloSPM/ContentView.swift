04e186f795f2cf2f68358abe5fa29de9699aef08
langpeu <icopy@naver.com>
2024-07-04 17:33:50 +0900

    feat: SPM

--- HelloSPM/ContentView.swift
+++ HelloSPM/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import KeychainSwift
+
 
 struct ContentView: View {
+    @State private var keyText: String = ""
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(keyText)
         }
         .padding()
+        .onAppear() {
+            let keychain = KeychainSwift()
+            keyText = keychain.get("my key")!
+            if keyText != "" {
+                keychain.set("Hello ++ world", forKey: "my key")
+            }
+            
+            
+        }
     }
 }
 
